9a07ac54c85bbc7075e9b20d059e239d54c0381d
SupahJay <SupahJay@users.noreply.github.com>
2018-08-04 19:53:20 -0300

    start

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+window.onload = () =>{
 Barba.Dispatcher.on('newPageReady', function() {
     window.location.pathname == "/portifolio.html"? portifolio():clean();
 /*document.getElementById("script")? */
@@ -5,8 +6,13 @@ Barba.Dispatcher.on('newPageReady', function() {
 function portifolio(){   
 document.body.style.background = '#fdfdfd';
 document.body.style.paddingTop = '90px';
+scrollBy(0, localStorage.getItem("scrollPos"));
+window.onscroll = () => {localStorage.setItem("scrollPos", scrollY)}
 }
 function clean(){   
 document.body.style.background = '';
 document.body.style.paddingTop = '';
+window.onscroll = () => {}
+}
+localStorage.setItem("scrollPos", "0")
 }
