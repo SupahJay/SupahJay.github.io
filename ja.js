9e7c2d6286f8c7fbdc753875b99483215de63246
SupahJay <SupahJay@users.noreply.github.com>
2018-08-04 19:06:11 -0300

    testy

---
+++ ja.js
@@ -0,0 +1,7 @@
+Barba.Dispatcher.on('newPageReady', function() {
+    window.pathname == "/Barba-test2.html"? alert("yes"):alert(window.pathname)
+/*document.getElementById("script")? */
+});
+/*function portifolio(){   
+document.body.style.display = 'block';
+}*/
